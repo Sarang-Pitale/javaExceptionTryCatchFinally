6f3a41b3a5388838689f14099d8ad3f00176eaf3
Sarang <sarangp@celcomsolutions.com>
2024-01-12 16:21:37 +0530

    Initial Commit

---
+++ src/com/sample/Arith.java
@@ -0,0 +1,9 @@
+package com.sample;
+
+public class Arith {
+    void div(int a, int b){
+        System.out.println("Division of the two values is: "+a/b);
+        System.out.println("WORK");
+    }
+
+}
